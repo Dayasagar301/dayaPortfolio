2fc7e7dc9f72d1f7065bae1b3d023b629bb86e61
Dayasagar <dayasagar301@gmail.com>
2024-05-22 18:12:45 +0530

    project uploaded

--- index.js
+++ index.js
@@ -13,41 +13,36 @@ navLinks.forEach((navLinks) => {
   });
 });
 
-
 function openResume() {
   window.open(
-    "https://drive.google.com/file/d/1R20kqgjwJdHY3e-NALLNrvAUlEB07hW4/view?usp=sharing",
-    '_blank'
+    "https://drive.google.com/file/d/1S2FF7aFndb_oXJY2FrSKf4scG1h1AbuE/view?usp=drive_link",
+    "_blank"
   );
 }
 
 function paddingFunction() {
-  let x = doccument.getElementById("home")
-  x.style.padding = "100px"
+  let x = doccument.getElementById("home");
+  x.style.padding = "100px";
 }
 
-document.querySelector("form").addEventListener("submit", saveFile );
-
-
+document.querySelector("form").addEventListener("submit", saveFile);
 
 function saveFile(e) {
+  e.preventDefault();
 
-  e.preventDefault()
+  let Formdata = JSON.parse(localStorage.getItem("portfolioData")) || [];
 
+  let name = document.getElementById("fname").value;
+  let email = document.getElementById("email").value;
+  let text = document.getElementById("subject").value;
 
-  let Formdata = JSON.parse(localStorage.getItem("portfolioData")) || []
-
-  let name=document.getElementById("fname").value
-  let email=document.getElementById("email").value
-  let text=document.getElementById("subject").value
-  
   let data = {
-    "name": name,
-    "email": email,
-    "text": text
-  }
-  Formdata.push(data)
-  localStorage.setItem("portfolioData", JSON.stringify(Formdata))
+    name: name,
+    email: email,
+    text: text,
+  };
+  Formdata.push(data);
+  localStorage.setItem("portfolioData", JSON.stringify(Formdata));
 }
 
 // document.addEventListener("DOMContentLoaded", function() {
@@ -82,41 +77,40 @@ function saveFile(e) {
 //   });
 // });
 
-
 // Get all the section elements
 // const sections = document.querySelectorAll('section');
-const sections = document.querySelectorAll("#home, #about, #skills, #projects, #github, #contact, #footer");
-
+const sections = document.querySelectorAll(
+  "#home, #about, #skills, #projects, #github, #contact, #footer"
+);
 
 // Get all the navigation links
-const navScroll = document.querySelectorAll('#nav-bar a');
+const navScroll = document.querySelectorAll("#nav-bar a");
 
 // Function to highlight the current section in the navigation menu
 function highlightNavLink() {
-    let index = sections.length;
+  let index = sections.length;
 
-    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
-    navScroll.forEach(link => link.classList.remove('active'));
-    navScroll[index].classList.add('active');
+  navScroll.forEach((link) => link.classList.remove("active"));
+  navScroll[index].classList.add("active");
 }
 
 // Add event listener for scroll
-window.addEventListener('scroll', highlightNavLink);
-
+window.addEventListener("scroll", highlightNavLink);
 
 function typeWriterEffect() {
-  const title = document.getElementById('title');
+  const title = document.getElementById("title");
   const text = title.innerText;
-  title.innerText = ''; // Clear the text content
+  title.innerText = ""; // Clear the text content
   let charIndex = 0;
 
   function type() {
-      if (charIndex < text.length) {
-          title.textContent += text.charAt(charIndex);
-          charIndex++;
-          setTimeout(type, 100); // Adjust typing speed (milliseconds)
-      }
+    if (charIndex < text.length) {
+      title.textContent += text.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 100); // Adjust typing speed (milliseconds)
+    }
   }
   type();
 }
@@ -130,4 +124,4 @@ function animateAndType() {
 }
 
 // Trigger typewriter effect after the page loads
-window.addEventListener('load', animateAndType);
+window.addEventListener("load", animateAndType);
